69a553e2c00cd3ab752ffc117a8d4403cc5414d7
exe-dealer <exe-dealer@yandex.ru>
2014-05-05 20:34:23 +0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+#knockout-flatBindingProvider
+
+```html
+<label data-bind-css.checked="isChecked">
+    <input data-bind-checked="isChecked"
+           data-bind-checked_value="$data"
+           type="checkbox" />
+    <span data-bind-text="name"></span>
+</label>
+```
---
+++ knockout-flatBindingProvider.js
@@ -0,0 +1,76 @@
+ko.flatBindingProvider = function () {
+    ko.flatBindingProvider._super.constructor.apply(this, arguments);
+}
+
+ko.flatBindingProvider._super = ko.bindingProvider.prototype;
+ko.flatBindingProvider.prototype = (function () {
+    var f = function () {};
+    f.prototype = ko.flatBindingProvider._super;
+    return new f();
+})();
+ko.flatBindingProvider.prototype.constructor = ko.flatBindingProvider;
+
+ko.flatBindingProvider.prototype._getNodeBindingsString = function (node) {
+    if (node.nodeType !== 1 /* element */) return;
+
+    var attrs = node.attributes;
+    var bindingObj = {};
+    for (var i = attrs.length - 1; i >= 0; i--) {
+        var attr = attrs[i];
+        if (attr.name.startsWith('data-bind-')) {
+            var proppath = attr.name
+                .slice(10) // substring after 'data-bind-'
+                // underscore_case to camelCase
+                .replace(/_(.)/g, function (_, c) { return c.toUpperCase(); })
+                .split('.');
+            var targetObj = bindingObj;
+            for (var j = 0; j < proppath.length - 1; j++) {
+                var propname = proppath[j];
+                targetObj = propname in targetObj ? targetObj[propname] : (targetObj[propname] = {});
+            }
+            targetObj[proppath[proppath.length - 1]] = attr.value;
+        }
+    }
+
+    var tokens = [];
+    var stack = [bindingObj];
+    while (stack.length) {
+        var obj = stack.pop();
+        if (typeof obj === 'string') {
+            tokens.push(obj);
+        } else {
+            stack.push('}');
+            var notFirst = false;
+            for (propname in obj) {
+                if (notFirst) stack.push(',');
+                stack.push(obj[propname], ':', '"', propname, '"');
+                notFirst = true;
+            }
+            stack.push('{');
+        }
+    }
+
+    // trim root brackets
+    tokens.pop();
+    tokens.shift();
+
+    return tokens.join('');
+}
+
+ko.flatBindingProvider.prototype.getBindingsString = function (node, bindingContext) {
+    return this._getNodeBindingsString(node) ||
+        ko.flatBindingProvider._super.getBindingsString.apply(this, arguments);
+};
+
+
+ko.flatBindingProvider.prototype.nodeHasBindings = function (node) {
+    if (node.nodeType === 1 /* element */) {
+        var attrs = node.attributes;
+        for (var i = attrs.length - 1; i >= 0; i--) {
+            if (attrs[i].name.startsWith('data-bind-')) {
+                return true;
+            }
+        }
+    }
+    return ko.flatBindingProvider._super.nodeHasBindings.apply(this, arguments);
+};
